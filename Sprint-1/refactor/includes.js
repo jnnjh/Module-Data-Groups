9eb63f0930c81126039851c93693152bcf57b5b5
jnnjh <joannejhoieacar@gmail.com>
2026-01-12 20:29:29 +0100

    answered sprint1-refactor-includes

--- Sprint-1/refactor/includes.js
+++ Sprint-1/refactor/includes.js
@@ -1,11 +1,20 @@
 // Refactor the implementation of includes to use a for...of loop
 
+/*
 function includes(list, target) {
   for (let index = 0; index < list.length; index++) {
     const element = list[index];
     if (element === target) {
       return true;
     }
+  }
+  return false;
+}
+*/
+
+function includes(list, target) {
+  for(x of list) {
+    if(x === target) return true;
   }
   return false;
 }
