eeffb2bb4d7b2c2814b60b326cf1d314e3520af3
jnnjh <joannejhoieacar@gmail.com>
2026-01-17 15:59:30 +0100

    answered sprint-1-implement-dedupe

--- Sprint-1/implement/dedupe.js
+++ Sprint-1/implement/dedupe.js
@@ -1,1 +1,17 @@
-function dedupe() {}
+function dedupe(arr) {
+    if(arr.length === 0) return [];
+
+    else {
+        let newArr = [];
+        for(let i = 0; i < arr.length; i++){
+            if(newArr.includes(arr[i]) === false){
+                newArr.push(arr[i]);
+            }
+        }
+    console.log(newArr);
+    return newArr;
+    }
+    
+}
+
+module.exports = dedupe;
