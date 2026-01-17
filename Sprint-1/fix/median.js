18c42458666dcb15cfdbb71bb928a2c8134b5c39
jnnjh <joannejhoieacar@gmail.com>
2026-01-17 15:33:23 +0100

    answered sprint-1-fix-median

--- Sprint-1/fix/median.js
+++ Sprint-1/fix/median.js
@@ -5,10 +5,31 @@
 // Hint: Please consider scenarios when 'list' doesn't have numbers (the function is expected to return null)
 // or 'list' has mixed values (the function is expected to sort only numbers).
 
+function checkNaN(item) {
+    if(typeof item === "number") return true;
+}
+
+function sort(a,b) {
+    return a-b;
+}
+
 function calculateMedian(list) {
-  const middleIndex = Math.floor(list.length / 2);
-  const median = list.splice(middleIndex, 1)[0];
-  return median;
+    if(!Array.isArray(list)) return null;
+
+    const filteredList = list.filter(checkNaN);
+    if(filteredList.length === 0) return null;
+
+    const newList = filteredList.toSorted(sort);
+    if(newList.length % 2 !== 0) {
+        const middleIndex = Math.floor(newList.length / 2);
+        const median = newList.splice(middleIndex, 1)[0];
+        return median;
+    } else {
+        const middleIndex = newList.length / 2;
+        const median = (newList[middleIndex-1] + newList[middleIndex]) / 2;
+        return median;
+    }
+    
 }
 
 module.exports = calculateMedian;
